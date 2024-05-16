4e64d37c2e359f33acea4465e06ed09a3175124a
Gowtham Jayaraman <gowtham@Gowthams-MacBook-Air.local>
2024-05-16 17:53:29 +0530

    Fixed Run Issue

--- src/main/java/com/niq/activate/service/ProductService.java
+++ src/main/java/com/niq/activate/service/ProductService.java
@@ -1,13 +1,69 @@
 package com.niq.activate.service;
 
 import com.niq.activate.dto.ProductDto;
+import com.niq.activate.exceptions.BusinessException;
+import com.niq.activate.jpa.ProductRepository;
 import com.niq.activate.model.Product;
+import com.niq.activate.model.ProductShopperMap;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public interface ProductService {
-    ProductDto getProduct(Integer id);
-    List<Product> getProductByFilters(Integer shopperId, String category, String brand, Integer limit);
+public class ProductService{
+    @Autowired
+    ProductRepository productRepository;
+
+    public ProductDto getProduct(Integer id) {
+        return productRepository.findById(id).map(this::convertToDto).orElseThrow(() ->new BusinessException("Product not found"));
+    }
+
+    public List<Product> getProductByFilters(Integer shopperId, String category, String brand, Integer limit) {
+        limit = validateInput(shopperId, limit);
+        Specification<Product> productSpecification = getSpecification(shopperId, category, brand, limit);
+        Pageable pageLimit = PageRequest.of(0,limit);
+        return productRepository.findAll(productSpecification, pageLimit).stream().toList();
+    }
+
+    private Specification<Product> getSpecification(Integer shopperId, String category, String brand, Integer limit){
+        return(root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if(shopperId != null){
+                Join<Product, ProductShopperMap> shopperJoin = root.join("product");
+                predicates.add(criteriaBuilder.and(criteriaBuilder.equal(shopperJoin.get("shopper_id"), shopperId)));
+            }
+            if(category != null){
+                predicates.add(criteriaBuilder.and(criteriaBuilder.equal(criteriaBuilder.trim(root.get("category")), category.trim())));
+            }
+            if(brand != null){
+                predicates.add(criteriaBuilder.and(criteriaBuilder.equal(criteriaBuilder.trim(root.get("brand")), brand.trim())));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    private Integer validateInput(Integer shopperId, Integer limit){
+        if(shopperId == null)
+            throw new BusinessException("ShopperId is Mandatory");
+        if(limit == null || limit < 10)
+            limit = 10;
+        if (limit > 100)
+            throw new BusinessException("Limit should be less than 100");
+        return limit;
+    }
+
+    private ProductDto convertToDto(Product product){
+        ProductDto dto = new ProductDto();
+        dto.setProductId(product.getId());
+        dto.setBrand(product.getBrand());
+        dto.setCategory(product.getCategory());
+        return dto;
+    }
 }
--- src/main/java/com/niq/activate/service/ShopperService.java
+++ src/main/java/com/niq/activate/service/ShopperService.java
@@ -1,9 +1,44 @@
 package com.niq.activate.service;
 
 import com.niq.activate.dto.PersonalizedProductDto;
+import com.niq.activate.dto.ProductRelevancyDto;
+import com.niq.activate.exceptions.BusinessException;
+import com.niq.activate.jpa.ShopperRepository;
+import com.niq.activate.model.ProductShopperMap;
+import com.niq.activate.model.Shopper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
-public interface ShopperService {
-    PersonalizedProductDto getPersonalizedProducts(Integer shopperId);
+public class ShopperService{
+
+    @Autowired
+    ShopperRepository shopperRepository;
+
+    public PersonalizedProductDto getPersonalizedProducts(Integer shopperId) {
+        Optional<Shopper> shopperOpt = shopperRepository.findById(shopperId);
+        if(shopperOpt.isEmpty()){
+            throw new BusinessException("Shopper not present in Database");
+        }
+        return convertToPersonalizedProductDto(shopperOpt.get().getProductShopperMap(), shopperId);
+    }
+
+    private PersonalizedProductDto convertToPersonalizedProductDto(Set<ProductShopperMap> productShopperMap, Integer shopperId){
+        PersonalizedProductDto dto = new PersonalizedProductDto();
+        dto.setShopperId(shopperId);
+        Set<ProductRelevancyDto> prdRelevancyDto = productShopperMap.stream().map(this::convertToProductRelevancyDto).collect(Collectors.toSet());
+        dto.setShelf(prdRelevancyDto);
+        return dto;
+    }
+
+    private ProductRelevancyDto convertToProductRelevancyDto(ProductShopperMap map){
+        ProductRelevancyDto dto = new ProductRelevancyDto();
+        dto.setProductId(map.getProduct().getId());
+        dto.setRelevancyScore(map.getRelevancyScore());
+        return dto;
+    }
 }
